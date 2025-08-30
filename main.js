681f99a066e67d6c825e7835ab9069b8a0134c07
M.Surenthiran <147502934+msurendhar@users.noreply.github.com>
2025-08-31 00:50:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["python Devolper","Elctronics Engineer","Frontend Developer", "Web Developer","IC Designer","Planning Engineer"],
+    typeSpeed : 18,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
